e83055b6bbb24944b24281fa4330f929d4d9f57a
System Administrator <root@macs-MBP.attlocal.net>
2022-06-15 16:19:49 -0400

    mom and dad

--- Project1rebuild/src/main/java/com/project1/services/P1_RequestMapper.java
+++ Project1rebuild/src/main/java/com/project1/services/P1_RequestMapper.java
@@ -17,7 +17,7 @@ public class P1_RequestMapper {
 	public void configureRoutes(Javalin app) {
 		
 		app.get("/hello", ctx -> {
-			ctx.result("hi Dad!");
+			ctx.result("hi Dad and Mom!");
 		});
 
 		// This will be for employee to view all their approved requests
